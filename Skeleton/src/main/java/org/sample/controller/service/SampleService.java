81f0bea1cd7a31bdb1c4534eb8ba5c7c2eda0c40
Manuel Grutsch <grutschm#gmail.com>
2015-10-23 13:40:35 +0200

    Added S

---
+++ Skeleton/src/main/java/org/sample/controller/service/SampleService.java
@@ -0,0 +1,15 @@
+package org.sample.controller.service;
+
+import java.util.List;
+
+import org.sample.controller.exceptions.InvalidUserException;
+import org.sample.controller.pojos.SignupForm;
+
+
+
+public interface SampleService {
+
+    public SignupForm saveFrom(SignupForm signupForm) throws InvalidUserException;
+    
+
+}
