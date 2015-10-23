ff2d59237dd23ade5936a070b6e15b551bd1ad92
Mario Hess <mario.hess@students.unibe.ch>
2015-10-23 19:09:16 +0200

    Added security with XML
    Login page is now working. Only hard coded users yet.

--- Skeleton/src/main/java/org/sample/controller/IndexController.java
+++ Skeleton/src/main/java/org/sample/controller/IndexController.java
@@ -10,10 +10,12 @@ import org.sample.controller.pojos.SignupForm;
 import org.sample.controller.service.SampleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -39,6 +41,30 @@ public class IndexController {
 //      	model.addObject("teamForm", new createTeamForm());
 //    	return model;
 //    }
+    
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String getLoginPage(@RequestParam(value="error", required=false) boolean error, 
+      ModelMap model) {
+    
+     // Add an error message to the model if login is unsuccessful
+     // The 'error' parameter is set to true based on the when the authentication has failed. 
+     // We declared this under the authentication-failure-url attribute inside the spring-security.xml
+     /* See below:
+      <form-login 
+       login-page="/krams/auth/login" 
+       authentication-failure-url="/krams/auth/login?error=true" 
+       default-target-url="/krams/main/common"/>
+      */
+     if (error == true) {
+      // Assign an error message
+      model.put("error", "You have entered an invalid username or password!");
+     } else {
+      model.put("error", "");
+     }
+      
+     // This will resolve to /WEB-INF/jsp/loginpage.jsp
+     return "login";
+    }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public ModelAndView create(@Valid SignupForm signupForm, BindingResult result, RedirectAttributes redirectAttributes) {
--- Skeleton/src/main/webapp/pages/login.jsp
+++ Skeleton/src/main/webapp/pages/login.jsp
@@ -4,8 +4,12 @@
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 
 
+
+
+
 <c:import url="template/header.jsp" />
 
+<!--
 
 <c:url value="/login" var="loginUrl"/>
 <form action="${loginUrl}" method="post">       
@@ -32,5 +36,31 @@
         value="${_csrf.token}"/>
     <button type="submit" class="btn">Log in</button>
 </form>
+
+-->
+
+<title>Insert title here</title>
+</head>
+<body>
+ 
+<h1>
+ 
+Login</h1><div id="login-error">
+
+${error}</div><form action="./j_spring_security_check" method="post" >
+ 
+ 
+ 
+<p>
+ 
+ 
+ <label for="j_username">Username</label>
+ <input id="j_username" name="j_username" type="text" />
+</p><p>
+ 
+ 
+ <label for="j_password">Password</label>
+ <input id="j_password" name="j_password" type="password" />
+</p><input  type="submit" value="Login"/> 
 
 <c:import url="template/footer.jsp" />
