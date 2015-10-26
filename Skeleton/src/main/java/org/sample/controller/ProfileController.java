5aeb2c4290fd2dec6ef020bf3c7748e070e8282f
Mario Hess <mario.hess@students.unibe.ch>
2015-10-26 11:36:05 +0100

    renamed security package

--- Skeleton/src/main/java/org/sample/controller/ProfileController.java
+++ Skeleton/src/main/java/org/sample/controller/ProfileController.java
@@ -19,6 +19,8 @@ public class ProfileController {
 	public ModelAndView modifyUser(@Valid ModifyUserForm form, BindingResult result, RedirectAttributes redirectAttributes){
 		
 		System.out.println(form.getPasswordControll());
+		
+		
 		return null;
 	}
 	
