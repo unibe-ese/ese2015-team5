80aaaacf6da0f8c02d9bf997ac7ef84172d9d2a5
Mario Hess <mario.hess@students.unibe.ch>
2015-11-09 22:37:59 +0100

    Added UserServiceTest back
    First test is working, extended test.xml

--- SkeletonWorking 2/src/main/java/org/sample/controller/service/test/UserServiceTest.java
+++ SkeletonWorking 2/src/main/java/org/sample/controller/service/test/UserServiceTest.java
@@ -3,15 +3,20 @@ package org.sample.controller.service.test;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.sample.controller.pojos.RegistrationForm;
+import org.junit.runner.RunWith;
 import org.sample.controller.pojos.SignupForm;
 import org.sample.controller.service.UserService;
 import org.sample.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/test.xml"})
 public class UserServiceTest {
 
-
+	@Autowired
 	private UserService testUserService;
 
 	@Test
@@ -26,7 +31,7 @@ public class UserServiceTest {
 		testSignupForm.setFirstName("John");
 		testSignupForm.setLastName("Doe");
 		testSignupForm.setPassword("asdf");
-		testSignupForm.setProfilePic(new MockMultipartFile("testPic", byteArray));
+		testSignupForm.setProfilePic(testPic);
 
 		User testUser = testUserService.saveUser(testSignupForm);
 		
@@ -34,7 +39,12 @@ public class UserServiceTest {
 		assertEquals("John", testUser.getFirstName());
 		assertEquals("Doe", testUser.getLastName());
 		assertEquals("asdf", testUser.getPassword());
-		assertEquals(null, testUser.getPic());
+		assertEquals(testPic.getBytes(), testUser.getPic().getFile());
+	}
+	
+	@Test
+	public void testUpdateUser(){
+		
 	}
 	
 }
