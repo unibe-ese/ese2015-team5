ea96de8c2edcee6828e992457f5de00a098f6da8
Mario Hess <mario.hess@students.unibe.ch>
2015-11-28 11:04:53 +0100

    Improved search on index

--- StutorV1/src/main/java/org/sample/controller/IndexController.java
+++ StutorV1/src/main/java/org/sample/controller/IndexController.java
@@ -1,11 +1,9 @@
 package org.sample.controller;
 
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
-
-import org.sample.model.Course;
-
-import java.util.*;
 
 import org.mortbay.util.ajax.AjaxFilter.AjaxResponse;
 import org.sample.controller.pojos.NewsFeedArticleInterface;
@@ -49,38 +47,36 @@ public class IndexController {
      * @return {@link ModelAndView} of index.
      */
     @RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
-    public ModelAndView index() {
+    public ModelAndView index(Model modelo) {  	
     	ModelAndView model = buildIndexModel();
-    	
+    	model.addAllObjects(modelo.asMap());
+    	System.out.println("entered Index");
     	assert model != null;
         return model;
     }
     
     private ModelAndView buildIndexModel() {
     	ModelAndView model = new ModelAndView("index");   
-    	model.addObject("competences", compService.findCompetenceLike(""));
-    	
     	model.addObject("applications", appService.getFutureApplications());
-    	List<NewsFeedArticleInterface> courses = userService.buildNewsFeed();
-    	System.out.println(courses.toString());
-    	model.addObject("newsfeed", courses);
+    	List<NewsFeedArticleInterface> news = userService.buildNewsFeed();
+    	model.addObject("newsfeed", news);
     	return model;
 	}
 
-	/**
-     * Not used. Will be used, if the searchCompetenceLike redirects to the index page. 
-     * @param model
-     * @return
-     */
-    public ModelAndView index(Model model) {
-    	System.out.println("index");
-    	ModelAndView newModel = new ModelAndView("index", model.asMap());   
-    	if(!model.containsAttribute("competences")){
-    		newModel.addObject("competences", compService.findCompetenceLike(""));
-    	}
-    	assert newModel != null;
-        return newModel;
-    }
+//	/**
+//     * Not used. Will be used, if the searchCompetenceLike redirects to the index page. 
+//     * @param model
+//     * @return
+//     */
+//    public ModelAndView index(Model model) {
+//    	System.out.println("index");
+//    	ModelAndView newModel = new ModelAndView("index", model.asMap());   
+//    	if(!model.containsAttribute("competences")){
+//    		newModel.addObject("competences", compService.findCompetenceLike(""));
+//    	}
+//    	assert newModel != null;
+//        return newModel;
+//    }
     
     @RequestMapping(value = "access-denied", method = RequestMethod.GET)
     public String accessDenied() {
@@ -99,12 +95,10 @@ public class IndexController {
      * @return A {@link ModelAndView} of the index page. 
      */
     @RequestMapping(value="/findCompetenceLike", method=RequestMethod.GET)
-    public ModelAndView findCompetenceLike(HttpServletRequest request){
+    public String findCompetenceLike(HttpServletRequest request, RedirectAttributes redirAttribtues){
     	String searchQuery = request.getParameter("searchQuery");
-    	ModelAndView model = new ModelAndView("index");
-    	model.addObject("competences", compService.findCompetenceLike(searchQuery));
-    	assert model != null;
-    	return model;
+    	redirAttribtues.addFlashAttribute("competences", compService.findCompetenceLike(searchQuery));
+    	return "redirect:/index";
     }
     
     @RequestMapping(value = "/security-error", method = RequestMethod.GET)
@@ -112,13 +106,7 @@ public class IndexController {
         redirectAttributes.addFlashAttribute("page_error", "You do have permission to do that!");
         return "redirect:/";
     }
-    
-    @RequestMapping(value="/ajaxTest", method = RequestMethod.GET)
-    public AjaxResponse ajaxTest(){
-    	System.out.println("works on this end!");
-		return null;
-  
-    }
+
     
     
 
