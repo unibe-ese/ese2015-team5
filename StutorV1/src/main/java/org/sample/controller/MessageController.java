17e540a7fb799f60dd38c3b9def76f26158b5eec
Steve Muerset <steve.muerset@students.unibe.ch>
2015-12-01 15:03:12 +0100

    nothing to commit

--- StutorV1/src/main/java/org/sample/controller/MessageController.java
+++ StutorV1/src/main/java/org/sample/controller/MessageController.java
@@ -6,6 +6,7 @@ import org.sample.controller.service.UserService;
 import org.sample.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,7 @@ public class MessageController {
 	MessageService msgService;
 	
 	@RequestMapping(value="/messages", method=RequestMethod.GET)
-	public ModelAndView showMessages(){
+	public ModelAndView showMessages(Model m){
 		User user = userService.getCurrentUser();
 		
 		
